a65277c0d62f7dce5d5d9e1f48feb3ba2f47c622
paviethra <62457057+paviethra@users.noreply.github.com>
2022-10-04 17:55:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const port = 3000;
+const router = express.Router();
+const path = require('path');
+const app = express();
+const nlp = require('compromise');
+nlp.extend(require('compromise-sentences'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+router.get('/past-tense', (req, res) => {
+    let doc = nlp(req.query.sentence);
+    doc.sentences().toPastTense();
+    const text = doc.text();    
+    res.json({ text });
+});
+
+app.use('/', router);
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+});
